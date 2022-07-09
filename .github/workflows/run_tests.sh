8b6bfdbfe57c77e3ec5373281d8857a0225b85a7
Hamid Ghadyani <hamid@hamid.cc>
2022-07-09 06:25:58 -0400

    Preparing for 'cargo publish'

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export RUST_TEST_NOCAPTURE=1
+if [ -n "$CARGO_REGISTRY_TOKEN" ]; then
+  echo "${CARGO_REGISTRY_TOKEN:1:3}"
+else
+  echo "didn't get CARGO_REGISTRY_TOKEN!"
+fi
+exit
 
 cargo test --features updater --lib -- --test-threads=1
 # cargo test --features updater,ci --lib -- --test-threads=1
