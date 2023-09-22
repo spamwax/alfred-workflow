caefd0350532eb33b2acf2e6fac2d30239a6d328
Hamid Ghadyani <hamid@hamid.cc>
2023-09-22 02:02:58 -0400

    Update github actions to v4
    - Run full tests in main.yml

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -8,13 +8,15 @@ else
 fi
 exit
 
-cargo test --features updater --lib -- --test-threads=1
+cargo update
+cargo test -- --test-threads=1 --nocapture
+cargo test --features updater --lib -- --test-threads=1 --nocapture
 # cargo test --features updater,ci --lib -- --test-threads=1
 
 # cargo test --features updater --lib -- --ignored --test-threads=1
 # cargo test --features updater,ci --lib -- --ignored --test-threads=1
 
 # doc tests
-cargo test --features updater --doc
+# cargo test --features updater --doc
 # cargo test --features updater,ci --doc
 
